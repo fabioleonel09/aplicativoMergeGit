07653dc5f02169db2008bb544f3605fa858e972c
Fábio Leonel <fabioleonelfono@gmail.com>
2023-07-29 20:36:44 -0300

    Aplicativo Merge Git: modificações iniciais de teste.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.lblTitulo = new System.Windows.Forms.Label();
-            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnEntrarFormA = new System.Windows.Forms.Button();
+            this.btnEntrarFormB = new System.Windows.Forms.Button();
+            this.btnEntrarFormC = new System.Windows.Forms.Button();
+            this.btnEntrarFormD = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -42,22 +45,55 @@
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Sistema de teste para Merge - Git Desktop";
             // 
-            // btnEntrar
+            // btnEntrarFormA
             // 
-            this.btnEntrar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEntrar.Location = new System.Drawing.Point(231, 304);
-            this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(131, 67);
-            this.btnEntrar.TabIndex = 1;
-            this.btnEntrar.Text = "Entrar";
-            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrarFormA.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrarFormA.Location = new System.Drawing.Point(27, 280);
+            this.btnEntrarFormA.Name = "btnEntrarFormA";
+            this.btnEntrarFormA.Size = new System.Drawing.Size(131, 67);
+            this.btnEntrarFormA.TabIndex = 1;
+            this.btnEntrarFormA.Text = "Entrar Form A";
+            this.btnEntrarFormA.UseVisualStyleBackColor = true;
+            // 
+            // btnEntrarFormB
+            // 
+            this.btnEntrarFormB.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrarFormB.Location = new System.Drawing.Point(164, 280);
+            this.btnEntrarFormB.Name = "btnEntrarFormB";
+            this.btnEntrarFormB.Size = new System.Drawing.Size(131, 67);
+            this.btnEntrarFormB.TabIndex = 2;
+            this.btnEntrarFormB.Text = "Entrar Form B";
+            this.btnEntrarFormB.UseVisualStyleBackColor = true;
+            // 
+            // btnEntrarFormC
+            // 
+            this.btnEntrarFormC.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrarFormC.Location = new System.Drawing.Point(301, 280);
+            this.btnEntrarFormC.Name = "btnEntrarFormC";
+            this.btnEntrarFormC.Size = new System.Drawing.Size(131, 67);
+            this.btnEntrarFormC.TabIndex = 3;
+            this.btnEntrarFormC.Text = "Entrar Form C";
+            this.btnEntrarFormC.UseVisualStyleBackColor = true;
+            // 
+            // btnEntrarFormD
+            // 
+            this.btnEntrarFormD.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrarFormD.Location = new System.Drawing.Point(438, 280);
+            this.btnEntrarFormD.Name = "btnEntrarFormD";
+            this.btnEntrarFormD.Size = new System.Drawing.Size(131, 67);
+            this.btnEntrarFormD.TabIndex = 4;
+            this.btnEntrarFormD.Text = "Entrar Form D";
+            this.btnEntrarFormD.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(597, 408);
-            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.btnEntrarFormD);
+            this.Controls.Add(this.btnEntrarFormC);
+            this.Controls.Add(this.btnEntrarFormB);
+            this.Controls.Add(this.btnEntrarFormA);
             this.Controls.Add(this.lblTitulo);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,7 +105,10 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnEntrarFormA;
+        private System.Windows.Forms.Button btnEntrarFormB;
+        private System.Windows.Forms.Button btnEntrarFormC;
+        private System.Windows.Forms.Button btnEntrarFormD;
     }
 }
 
